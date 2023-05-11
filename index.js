91b44c85de08fa1cfb6c38d7646ffdafc7a8c1bf
habiyanlazuard21 <habiyanlazuard21@gmail.com>
2023-05-11 21:03:56 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+ const express = require('express');
+ const app = express();
+
+ app.get("/", (req, res)=> {
+    return res.json({
+        message: 'response sucesssfully ',
+        author: "habbyan lazuard"
+    });
+ });
+
+ app.get("/users", (res, req)=> {
+   return res.send("successfully");
+ });
+
+ app.listen(8000, ()=> {
+    console.log('proccess running at port 8000 ');
+ });
