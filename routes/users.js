102d2e03a039549d7672402b60b9dab9d4f17b9a
habiyanlazuard21 <habiyanlazuard21@gmail.com>
2023-05-12 17:12:17 +0700

    adding routes user

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,13 @@ const userControllers = require('../controller/userController');
 /* GET users listing. */
 router.get('/auth',verifyToken, userControllers.getAllUsers);
 
+/*post */
+router.get('/:userId',verifyToken, userControllers.getUserById);
+
+//update users
+router.put('/:userId',verifyToken, userControllers.updateUser);
+
+//delete
+router.delete('/:userId',verifyToken, userControllers.deleteUser);
+
 module.exports = router;
