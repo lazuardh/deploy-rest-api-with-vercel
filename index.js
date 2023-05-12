4b313cf5d975b3c47950994fed87e1ea18a99069
habiyanlazuard21 <habiyanlazuard21@gmail.com>
2023-05-12 17:43:11 +0700

    adding routes auth in vercel

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
  require("./bin/www");
- require('dotenv').config();
  const { config } = require("dotenv");
  const express = require('express');
  const app = express();
@@ -8,13 +7,13 @@
 
  app.get("/", (req, res)=> {
     return res.json({
-        message: 'response sucesssfully ',
+        message: 'ok',
         author: "habbyan lazuard"
     });
  });
 
  app.use("/users", userRoutes);
 
- app.listen(process.env.PORT, ()=> {
+ app.listen(8000, ()=> {
     console.log('proccess running at port successfull')
  });
--- routes/users.js
+++ routes/users.js
@@ -4,15 +4,8 @@ const verifyToken = require('../middleware/verify_token');
 const userControllers = require('../controller/userController');
 
 /* GET users listing. */
-router.get('/auth',verifyToken, userControllers.getAllUsers);
-
-/*post */
-router.get('/:userId',verifyToken, userControllers.getUserById);
-
-//update users
-router.put('/:userId',verifyToken, userControllers.updateUser);
-
-//delete
-router.delete('/:userId',verifyToken, userControllers.deleteUser);
+router.get('/auth',verifyToken, function(req, res, next) {
+  res.send('respond with a resource');
+});
 
 module.exports = router;
